0fa9c3d4dfa5a3a299b102944cab99c5291fd517
marc2609m <marcmm2609@gmail.com>
2024-04-30 19:32:05 +0200

    Primer Commit

---
+++ src/main/java/com/marc_martinez/PracticaM6MarcMartinezFase1Application.java
@@ -0,0 +1,13 @@
+package com.marc_martinez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaM6MarcMartinezFase1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaM6MarcMartinezFase1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/marc_martinez/PracticaM6MarcMartinezFase1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marc_martinez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaM6MarcMartinezFase1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
